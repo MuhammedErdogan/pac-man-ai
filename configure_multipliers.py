885ff409ca8629d15b9197e89a2b1bfe3adcd9c6
Hazar Belge <hazarbelge@gmail.com>
2023-01-03 21:40:50 +0300

    Update configure multipliers

--- configure_multipliers.py
+++ configure_multipliers.py
@@ -1,3 +1,4 @@
+import random
 import subprocess
 
 scoreMultiplier = 1.0
@@ -11,11 +12,15 @@ distanceToClosestScaredGhostMultiplier = 1.0
 
 def decimal_range():
     start = float(1.0)
-    stop = float(10.0)
-    increment = float(0.5)
+    stop = float(100.0)
+    increment = float(1)
     while start < stop:
         yield start
         start += increment
+
+
+def return_float_between_1_and_100():
+    return float(random.random() * 100.0 + 1.0).__round__(2)
 
 
 if __name__ == '__main__':
@@ -23,42 +28,39 @@ if __name__ == '__main__':
     max_avg_score_win_rate = ""
     max_avg_score_params = []
 
-    game_count = 30
+    game_count = 50
 
-    for i in decimal_range():
-        scoreMultiplier = i
-        for j in decimal_range():
-            numberOfFoodsLeftMultiplier = j
-            for k in decimal_range():
-                numberOfCapsulesLeftMultiplier = k
-                for l in decimal_range():
-                    distanceToClosestFoodMultiplier = l
-                    for m in decimal_range():
-                        distanceToClosestCapsuleMultiplier = m
-                        for n in decimal_range():
-                            distanceToClosestActiveGhostMultiplier = n
-                            for o in decimal_range():
-                                distanceToClosestScaredGhostMultiplier = o
+    for i in range(1000):
+        print("Game Number: " + str(i+1))
 
-                                returned_output = subprocess.check_output(
-                                    f"python3 pacman.py -l smallClassic -p ExpectimaxAgent -a evalFn=better -q -n {game_count}",
-                                    shell=True)
-                                returned_output = returned_output.decode("utf-8")
-                                score = float(returned_output.split("\n")[game_count].split(":")[1].strip())
-                                win_rate = returned_output.split("Win Rate: ")[1].split("%")[0].split("\n")[0].strip()
+        scoreMultiplier = return_float_between_1_and_100()
+        numberOfFoodsLeftMultiplier = return_float_between_1_and_100()
+        numberOfCapsulesLeftMultiplier = return_float_between_1_and_100()
+        distanceToClosestFoodMultiplier = return_float_between_1_and_100()
+        distanceToClosestCapsuleMultiplier = return_float_between_1_and_100()
+        distanceToClosestActiveGhostMultiplier = return_float_between_1_and_100()
+        distanceToClosestScaredGhostMultiplier = return_float_between_1_and_100()
 
-                                if score > max_avg_score:
-                                    max_avg_score = score
-                                    max_avg_score_win_rate = win_rate
-                                    max_avg_score_params = [scoreMultiplier, numberOfFoodsLeftMultiplier,
-                                                            numberOfCapsulesLeftMultiplier,
-                                                            distanceToClosestFoodMultiplier,
-                                                            distanceToClosestCapsuleMultiplier,
-                                                            distanceToClosestActiveGhostMultiplier,
-                                                            distanceToClosestScaredGhostMultiplier]
-                                    print('Max Avg Score: ', max_avg_score)
-                                    print('Max Avg Score Win Rate: ', max_avg_score_win_rate)
-                                    print('Max Avg Score Params: ', max_avg_score_params)
+        returned_output = subprocess.check_output(
+            f"python3 pacman.py -l smallClassic -p ExpectimaxAgent -a evalFn=better -q -n {game_count}",
+            shell=True)
+        returned_output = returned_output.decode("utf-8")
+        score = float(returned_output.split("\n")[game_count].split(":")[1].strip())
+        win_rate = returned_output.split("Win Rate: ")[1].split("%")[0].split("\n")[0].strip()
+
+        if score > max_avg_score:
+            max_avg_score = score
+            max_avg_score_win_rate = win_rate
+            max_avg_score_params = [scoreMultiplier,
+                                    numberOfFoodsLeftMultiplier,
+                                    numberOfCapsulesLeftMultiplier,
+                                    distanceToClosestFoodMultiplier,
+                                    distanceToClosestCapsuleMultiplier,
+                                    distanceToClosestActiveGhostMultiplier,
+                                    distanceToClosestScaredGhostMultiplier]
+            print('Max Avg Score: ', max_avg_score)
+            print('Max Avg Score Win Rate: ', max_avg_score_win_rate)
+            print('Max Avg Score Params: ', max_avg_score_params)
 
     print('Max Avg Score: ', max_avg_score)
     print('Max Avg Score Win Rate: ', max_avg_score_win_rate)
